8c99bd13c4709f122542359bbbbcaf0d8bdba5ae
jiwon <pg15320@gmail.com>
2023-04-11 21:24:37 +0900

    init

---
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/AcademicAdminApplication.java
@@ -0,0 +1,13 @@
+package com.SoftwareEngineering.AcademicAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoftwareEngineering/AcademicAdmin/AcademicAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareEngineering.AcademicAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
