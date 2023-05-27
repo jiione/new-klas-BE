17ca8157a8a0c149a773cf29093c1c73a4d9eb15
Icln <gksmfthsus35@naver.com>
2023-05-27 18:06:38 +0900

    feat: 서비스 작성

--- src/main/java/com/SoftwareEngineering/AcademicAdmin/service/UserService.java
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/service/UserService.java
@@ -3,10 +3,13 @@ package com.SoftwareEngineering.AcademicAdmin.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.SoftwareEngineering.AcademicAdmin.dto.request.LoginReqDTO;
 import com.SoftwareEngineering.AcademicAdmin.dto.request.SignUpReqDTO;
 import com.SoftwareEngineering.AcademicAdmin.entity.Department;
 import com.SoftwareEngineering.AcademicAdmin.entity.User;
 import com.SoftwareEngineering.AcademicAdmin.exception.department.DepartmentNotFound;
+import com.SoftwareEngineering.AcademicAdmin.exception.user.InvalidPassword;
+import com.SoftwareEngineering.AcademicAdmin.exception.user.UserNotFound;
 import com.SoftwareEngineering.AcademicAdmin.repository.DepartmentRepository;
 import com.SoftwareEngineering.AcademicAdmin.repository.UserRepository;
 
@@ -18,6 +21,20 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 	private final UserRepository userRepository;
 	private final DepartmentRepository departmentRepository;
+
+	public void login(LoginReqDTO loginReqDTO){
+		User user = getUser(loginReqDTO.getStudentId());
+		validateUser(user, loginReqDTO.getPassword());
+	}
+
+	private void validateUser (User user, String password){
+		if(!user.getPassword().equals(password))
+			throw new InvalidPassword();
+	}
+	private User getUser(Long studentId){
+		return userRepository.findUserByStudentId(studentId)
+			.orElseThrow(UserNotFound::new);
+	}
 
 	public void signUp(SignUpReqDTO signUpReqDTO){
 		Department department = getDepartmentByName(signUpReqDTO.getDepartmentName());
