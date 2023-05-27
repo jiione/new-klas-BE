65ba7f364920c2d99e0143efb90805db261cb39a
Icln <gksmfthsus35@naver.com>
2023-05-27 18:06:29 +0900

    feat: 쿼리 작성

--- src/main/java/com/SoftwareEngineering/AcademicAdmin/repository/UserRepository.java
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/repository/UserRepository.java
@@ -1,8 +1,17 @@
 package com.SoftwareEngineering.AcademicAdmin.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.SoftwareEngineering.AcademicAdmin.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query(value = "SELECT u "
+		+ "FROM User  u "
+		+ "WHERE u.studentId = :studentId")
+	Optional<User> findUserByStudentId(@Param("studentId")Long studentId);
 }
