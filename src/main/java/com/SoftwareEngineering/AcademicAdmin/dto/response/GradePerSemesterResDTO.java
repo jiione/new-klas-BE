0df688993ba9c037042928e7b39a03b649ff9331
jiwon <pg15320@gmail.com>
2023-05-29 12:48:44 +0900

    Service 로직

---
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/dto/response/GradePerSemesterResDTO.java
@@ -0,0 +1,13 @@
+package com.SoftwareEngineering.AcademicAdmin.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GradePerSemesterResDTO {
+}
--- src/main/java/com/SoftwareEngineering/AcademicAdmin/service/GradeService.java
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/service/GradeService.java
@@ -1,11 +1,21 @@
 package com.SoftwareEngineering.AcademicAdmin.service;
 
+import com.SoftwareEngineering.AcademicAdmin.dto.response.GradePerSemesterResDTO;
+import com.SoftwareEngineering.AcademicAdmin.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class GradeService {
+
+    private final UserService userService;
+    public List<GradePerSemesterResDTO> getGradePerSemester(Long studentId){
+        User user = userService.getUser(studentId);
+        
+    }
 }
