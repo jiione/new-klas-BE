59e9685d613f72012d8f9117b427df17266ec6ac
Icln <gksmfthsus35@naver.com>
2023-06-01 11:34:26 +0900

    feat: DTO 추가

---
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/dto/response/Lecture/NoticeResDTO.java
@@ -0,0 +1,22 @@
+package com.SoftwareEngineering.AcademicAdmin.dto.response.Lecture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NoticeResDTO {
+
+	private List<NoticeDTO> notices = new ArrayList<>();
+
+	private NoticeResDTO (List<NoticeDTO> notices){
+		this.notices = notices;
+	}
+
+	public static NoticeResDTO from(List<NoticeDTO>notices){
+		return new NoticeResDTO(notices);
+	}
+}
