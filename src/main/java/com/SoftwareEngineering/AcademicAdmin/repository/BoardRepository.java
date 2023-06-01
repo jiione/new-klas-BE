bcb29ad4d1c1d4286eed51b1fde0c9fbdc7fed0a
Icln <gksmfthsus35@naver.com>
2023-06-01 11:34:40 +0900

    feat: 쿼리추가

--- src/main/java/com/SoftwareEngineering/AcademicAdmin/repository/BoardRepository.java
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/repository/BoardRepository.java
@@ -2,10 +2,21 @@ package com.SoftwareEngineering.AcademicAdmin.repository;
 
 import com.SoftwareEngineering.AcademicAdmin.entity.Board;
 import com.SoftwareEngineering.AcademicAdmin.entity.Post;
+import com.SoftwareEngineering.AcademicAdmin.entity.Subjects;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
+	@Query(value = "SELECT b "
+		+ "FROM Board b "
+		+ "JOIN FETCH b.subjects s "
+		+ "JOIN FETCH b.posts p "
+		+ "WHERE s.id = :classId AND b.code = 0 "
+		+ "ORDER BY p.time desc ")
+	Board findSubjectsByClassId(@Param("classId")Long classId);
 }
