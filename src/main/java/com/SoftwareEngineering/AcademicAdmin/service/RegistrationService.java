27f559501ab820a7ef674f0772baf8fb286c802d
Icln <gksmfthsus35@naver.com>
2023-06-04 17:39:19 +0900

    feat: 서비스 작성

--- src/main/java/com/SoftwareEngineering/AcademicAdmin/service/RegistrationService.java
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/service/RegistrationService.java
@@ -1,10 +1,16 @@
 package com.SoftwareEngineering.AcademicAdmin.service;
 
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.SoftwareEngineering.AcademicAdmin.dto.response.ScheduleDetailDTO;
+import com.SoftwareEngineering.AcademicAdmin.dto.response.registration.SearchDTO;
 import com.SoftwareEngineering.AcademicAdmin.dto.response.registration.SearchResDTO;
 import com.SoftwareEngineering.AcademicAdmin.entity.Subjects;
 import com.SoftwareEngineering.AcademicAdmin.repository.SubjectsRepository;
@@ -16,14 +22,23 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class RegistrationService {
 	private final LectureService lectureService;
+	private final StudentMainService studentMainService;
 	private final SubjectsRepository subjectsRepository;
 
 	public SearchResDTO getSearchSubject(Long studentId, Long year, Long semester ,String search){
 		lectureService.validateUser(studentId);
 		List<Subjects> subjects = subjectsRepository.findSubjectsByYearAndSemesterAndSearch(year, semester, search);
-		for (Subjects subject : subjects){
+		return getSearchResponse(subjects);
+	}
 
-		}
-		return null;
+	private SearchResDTO getSearchResponse(List<Subjects> subjects){
+		List<SearchDTO> searchDTOS = subjects.stream()
+			.map(subject -> {
+				List<ScheduleDetailDTO> scheduleDetailDTOS = studentMainService.getScheduleDetail(subject);
+				return SearchDTO.of(subject, scheduleDetailDTOS);
+			})
+			.collect(Collectors.toList());
+
+		return SearchResDTO.from(searchDTOS);
 	}
 }
