7b8a4e78630e53d0692810ba997c39e679949fa9
jiwon <pg15320@gmail.com>
2023-06-07 18:39:50 +0900

    [CREATE] file 다운로드

--- src/main/java/com/SoftwareEngineering/AcademicAdmin/controller/FileController.java
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/controller/FileController.java
@@ -1,16 +1,18 @@
 package com.SoftwareEngineering.AcademicAdmin.controller;
 
 import com.SoftwareEngineering.AcademicAdmin.entity.File;
+import com.SoftwareEngineering.AcademicAdmin.repository.FileRepository;
 import com.SoftwareEngineering.AcademicAdmin.service.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +20,28 @@ import java.io.IOException;
 public class FileController {
 
     private final FileService fileService;
+
+    private final FileRepository fileRepository;
+
     @PostMapping("/upload")
     public ResponseEntity<File> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("postId") Long id) throws IOException {
         File uploadedFile = fileService.uploadFile(file,id);
         return ResponseEntity.ok(uploadedFile);
+    }
+
+    @GetMapping("/download/{id}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable("id") Long id) {
+        Optional<File> optionalFileEntity = fileRepository.findById(id);
+        if (optionalFileEntity.isPresent()) {
+            File file = optionalFileEntity.get();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", file.getFileName());
+
+            return new ResponseEntity<>(file.getFileData(), headers, HttpStatus.OK);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/SoftwareEngineering/AcademicAdmin/entity/File.java
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/entity/File.java
@@ -2,6 +2,7 @@ package com.SoftwareEngineering.AcademicAdmin.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
 public class File {
 
     @Id
