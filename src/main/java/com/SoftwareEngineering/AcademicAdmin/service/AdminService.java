d1b72a0614ecf8f0c7c1a4460bef4f12687a96d9
Icln <gksmfthsus35@naver.com>
2023-06-09 17:19:28 +0900

    fix: 예외처리

--- src/main/java/com/SoftwareEngineering/AcademicAdmin/service/AdminService.java
+++ src/main/java/com/SoftwareEngineering/AcademicAdmin/service/AdminService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.SoftwareEngineering.AcademicAdmin.dto.response.AdminResDTO;
 import com.SoftwareEngineering.AcademicAdmin.dto.response.UserDTO;
 import com.SoftwareEngineering.AcademicAdmin.entity.User;
+import com.SoftwareEngineering.AcademicAdmin.exception.user.AdminNotDelete;
 import com.SoftwareEngineering.AcademicAdmin.exception.user.NotAdmin;
 import com.SoftwareEngineering.AcademicAdmin.repository.UserRepository;
 
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class AdminService {
 	private final UserRepository userRepository;
+	private final LectureService lectureService;
 
 	public AdminResDTO getUsers(Long adminId){
 		validateAdmin(adminId);
@@ -33,7 +35,15 @@ public class AdminService {
 
 	public void deleteUser(Long adminId, Long studentId){
 		validateAdmin(adminId);
+		lectureService.validateUser(studentId);
+		validateDeleteId(studentId);
 		userRepository.deleteByStudentId(studentId);
+	}
+
+
+	private void validateDeleteId(Long studentId){
+		if (studentId.equals(1111L))
+			throw new AdminNotDelete();
 	}
 
 	private void validateAdmin(Long adminId){
